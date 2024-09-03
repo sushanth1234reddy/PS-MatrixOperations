39b85492b442f1b6ab7b7e863218810cbef2718d
Singadwar Sushanth Reddy <76097402+sushanth1234reddy@users.noreply.github.com>
2024-09-03 18:16:29 +0530

    Add files via upload

---
+++ SameLineBorderElements.js
@@ -0,0 +1,27 @@
+let rows=+prompt("enter no.of rows:");
+let cols=+prompt("enter no of cols:");
+let MatrixA=[];
+let Tmat=[];
+for(i=1;i<=rows;i++){
+    x=[];
+    for(j=1;j<=cols;j++){
+        console.log("Enter"+i+" "+j+ "vales:");
+        el=+prompt();
+        x.push(el);
+    }
+    MatrixA.push(x)
+}
+for(i of MatrixA){
+    console.log(i.join(" "));
+}
+let inline="";
+for(i=0;i<rows;i++){
+    for(j=0;j<cols;j++){
+        if(i==0 || j==cols-1 || j==0 || i==rows-1 ){
+            
+        
+        inline=inline+MatrixA[i][j]+" ";
+        }
+    }
+}
+console.log("Displaying border elements:"+inline);
